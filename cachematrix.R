4178191f5a97fbf9549adadd03e71434c893dd7b
Ajay K M <amothukuri@sapient.com>
2017-05-27 19:13:29 +0530

    Update cachematrix.R
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,20 +6,30 @@
 
 makeCacheMatrix <- function(x = matrix()) {
     
+    ## Initialize empty matrices
+    ## Inverse matrix
     xInv <- matrix()
+    ## Matrix pertaining to the inverse matrix
     xCurr <- matrix()
     
     setMatrix <- function(y){
+        ## Compare new matrix to the current matrix
         if(!(dim(y) == dim(x)) || !all(x == y)){
+            ## Assign the new matrix
             x <<- y
+            ##Empty the Inverse matrix as new matrix is different from original
             xInv <<- matrix()
+            ##Prompt a message to calculate the inverse as the matrix changed
             message("Current Matrix changed, please calculate Inverse!!")
         }
     }
     
     setInvMatrix <- function(){
+        ##Compare the current matrix with matrix related to the inverse matrix 
         if(!(dim(x) == dim(xCurr)) || !all(xCurr == x)){
+            ## Calculate the inverse
             xInv <<- solve(x)
+            ## As the new matrix is different, modify this placeholder matrix
             xCurr <<- x
         }
     }
@@ -40,12 +50,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## modified, it re-created the inverse and caches it again.
 
 cacheSolve <- function(x, ...) {
+    
+    ## Get Inverse matrix
     xInv <- x$getInvMatrix()
+    
+    ## Check if Inverse is empty
     if(dim(xInv) == 1 && is.na(xInv[1,1])){
+        ## If empty calculate the new inverse matrix
         x$setInvMatrix()
         xInv <- x$getInvMatrix()
         return(xInv)
     }
+    
+    ## Retrieve the stored inverse matrix, as it is not empty!
     message("Getting cached data")
     return(xInv)
     
